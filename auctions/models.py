6c38721d34f248f04b747b32dff85c63ccb42fd1
Mitchell Huber <mitch@Mitchells-MBP.fios-router.home>
2020-08-05 21:17:32 -0700

    Created a page to add listing and started to implement listing page

---
+++ auctions/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+class Categories(models.Model):
+    category = models.CharField(max_length=20, default='IDK')
+
+    def __str__(self):
+        return f"{self.category}"
+
+class Listings(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    picture = models.URLField(null=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, null=True)
+
+    def __str_(self):
+        return f"{self.title} starting at {self.starting_bid}, {self.description} {self.picture} {self.category}"
